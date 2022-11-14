5690eabb12876f4715ec5dc47e91ce8b45e477c9
Leonardo <leo.araujogomes@hotmail.com>
2022-11-13 21:36:48 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+To use this program, all you need to do is start the index file with node, enter the wikipedia page's language and the title of the article. There you have, the a wikipedia text into a file.
---
+++ index.js
@@ -0,0 +1,56 @@
+const axios = require("axios");
+const fs = require("fs");
+const prompt = require("prompt-sync")();
+const path = __dirname + "/TextoConvertido.txt";
+
+function wikipediaExtract(language, title) {
+  const endpoint = `http://${language}.wikipedia.org/w/api.php`;
+  const params =
+    "?action=query" +
+    "&prop=extracts" +
+    `&titles=${title}` +
+    "&explaintext=1" +
+    "&format=json" +
+    "&formatversion=2";
+
+  const link = endpoint + params;
+
+  async function getResponse(url) {
+    return await axios
+      .get(url)
+      .catch((error) => console.log("An error has occurred: " + error))
+      .then((response) => {
+        return (
+          response.data.query.pages[0].title +
+          "\n\n" +
+          response.data.query.pages[0].extract
+        );
+      });
+  }
+
+  return getResponse(link);
+}
+
+function fileWriter(filePath, text) {
+  fs.writeFile(filePath, text, function (error) {
+    if (error) {
+      console.error("An error has occurred: " + error.message);
+    } else {
+      console.log("File successfully written to: " + filePath);
+    }
+  });
+}
+
+async function convertText() {
+  const language = prompt(
+    "Enter the language of the desired Wikipedia page (pt/en): "
+  );
+  const title = prompt(
+    "Enter the title of the desired Wikipedia page (text after wiki/ in the URL): "
+  );
+
+  var apiOutput = await wikipediaExtract(language, title);
+  fileWriter(path, apiOutput);
+}
+
+convertText();
